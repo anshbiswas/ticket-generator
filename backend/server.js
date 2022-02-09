2e5b138845481abe6d2329d9b6b4d870cc2f7c9b
anshbiswas <65728642+anshbiswas@users.noreply.github.com>
2022-02-09 17:25:46 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to the Support Desk API'})
-})
-
-
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
@@ -28,6 +23,7 @@ if(process.env.NODE_ENV === 'production') {
     //Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+    // FIX: below code fixes app crashing on refresh in deployment
     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
 } else {
     app.get('/', (req, res) => {
